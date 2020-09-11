c9a9dcbd207818c9ec2e584e99d64ad1a15b2302
1996aap <1996aap@gmail.com>
2020-09-11 10:25:36 -0300

    sem print

--- src/Compilador.java
+++ src/Compilador.java
@@ -58,152 +58,152 @@ public class Compilador extends MaquinaVirtual
 		switch (Lexema)
 		{
 		case "programa": // Carregar constante (passado por parametro)
-			System.out.println("Sprograma");
+
 			
 			MSimbolo.add("Sprograma");
 			break;
 		case "inicio": // Carregar constante (passado por parametro)
-			System.out.println("Sinicio");
+
 			MSimbolo.add("Sinicio");
 			break;
 		case "fim": // Carregar constante (passado por parametro)
-			System.out.println("Sfim");
+
 			MSimbolo.add("Sfim");
 			break;
 		case "procedimento": // Carregar constante (passado por parametro)
-			System.out.println("Sprocedimento");
+
 			MSimbolo.add("Sprocedimento");
 			break;
 		case "funcao": // Carregar constante (passado por parametro)
-			System.out.println("Sfuncao");
+
 			MSimbolo.add("Sfuncao");
 			break;
 		case "se": // Carregar constante (passado por parametro)
-			System.out.println("Sse");
+
 			MSimbolo.add("Sse");
 			break;
 		case "entao": // Carregar constante (passado por parametro)
-			System.out.println("Sentao");
+
 			MSimbolo.add("Sentao");
 			break;
 		case "senao": // Carregar constante (passado por parametro)
-			System.out.println("Ssenao");
+
 			MSimbolo.add("Ssenao");
 			break;
 		case "enquanto": // Carregar constante (passado por parametro)
-			System.out.println("Senquanto");
+
 			MSimbolo.add("Senquanto");
 			break;
 		case "faca": // Carregar constante (passado por parametro)
-			System.out.println("Sfaca");
+
 			MSimbolo.add("Sfaca");
 			break;
 		case ":=": // Carregar constante (passado por parametro)
-			System.out.println("Satribuição");
+
 			MSimbolo.add("Satribuição");
 			break;
 		case "escreva": // Carregar constante (passado por parametro)
-			System.out.println("Sescreva");
+
 			MSimbolo.add("Sescreva");
 			break;
 		case "leia": // Carregar constante (passado por parametro)
-			System.out.println("Sleia");
+
 			MSimbolo.add("Sleia");
 			break;
 		case "var": // Carregar constante (passado por parametro)
-			System.out.println("Svar");
+
 			MSimbolo.add("Svar");
 			break;
 		case "inteiro": // Carregar constante (passado por parametro)
-			System.out.println("Sinteiro");
+
 			MSimbolo.add("Sinteiro");
 			break;
 		case "booleano": // Carregar constante (passado por parametro)
-			System.out.println("Sbooleano");
+
 			MSimbolo.add("Sbooleano");
 			break;
 		case "indentificador": // Carregar constante (passado por parametro)
-			System.out.println("Sindentificador");
+
 			MSimbolo.add("Sindentificador");
 			break;
 		case "numero": // Carregar constante (passado por parametro)
-			System.out.println("Snúmero");
+
 			MSimbolo.add("Snúmero");
 			break;
 		case ".": // Carregar constante (passado por parametro)
-			System.out.println("Sponto");
+
 			MSimbolo.add("Sponto");
 			break;
 		case ";": // Carregar constante (passado por parametro)
-			System.out.println("Sponto_virgula");
+
 			MSimbolo.add("Sponto_virgula");
 			break;
 		case ",": // Carregar constante (passado por parametro)
-			System.out.println("Svirgula");
+
 			MSimbolo.add("Svirgula");
 			break;
 		case "(": // Carregar constante (passado por parametro)
-			System.out.println("Sabre_parenteses");
+
 			MSimbolo.add("Sabre_parenteses");
 			break;
 		case ")": // Carregar constante (passado por parametro)
-			System.out.println("Sfecha_parenteses");
+
 			MSimbolo.add("Sfecha_parenteses");
 			break;
 		case ">": // Carregar constante (passado por parametro)
-			System.out.println("Smaior");
+
 			MSimbolo.add("Smaior");
 			break;
 		case ">=": // Carregar constante (passado por parametro)
-			System.out.println("Smaiorig");
+
 			MSimbolo.add("Smaiorig");
 			break;
 		case "=": // Carregar constante (passado por parametro)
-			System.out.println("Sig");
+
 			MSimbolo.add("Sig");
 			break;
 		case "<": // Carregar constante (passado por parametro)
-			System.out.println("Smenor");
+
 			MSimbolo.add("Smenor");
 			break;
 		case "<=": // Carregar constante (passado por parametro)
-			System.out.println("Smenorig");
+
 			MSimbolo.add("Smenorig");
 			break;
 		case "!=": // Carregar constante (passado por parametro)
-			System.out.println("Sgif");
+
 			MSimbolo.add("Sgif");
 			break;
 		case "+": // Carregar constante (passado por parametro)
-			System.out.println("Smais");
+
 			MSimbolo.add("Smais");
 			break;
 		case "-": // Carregar constante (passado por parametro)
-			System.out.println("Smenos");
+
 			MSimbolo.add("Smenos");
 			break;
 		case "*": // Carregar constante (passado por parametro)
-			System.out.println("Smult");
+
 			MSimbolo.add("Smult");
 			break;
 		case "div": // Carregar constante (passado por parametro)
-			System.out.println("Sdiv");
+
 			MSimbolo.add("Sdiv");
 			break;
 		case "e": // Carregar constante (passado por parametro)
-			System.out.println("Se");
+
 			MSimbolo.add("Se");
 			break;
 		case "ou": // Carregar constante (passado por parametro)
-			System.out.println("Sou");
+
 			MSimbolo.add("Sou");
 			break;
 		case "nao": // Carregar constante (passado por parametro)
-			System.out.println("Snao");
+
 			MSimbolo.add("Snao");
 			break;
 		case ":": // Carregar constante (passado por parametro)
-			System.out.println("Sdoispontos");
+
 			MSimbolo.add("Sdoispontos");
 			break;
 			default:
@@ -226,7 +226,7 @@ public class Compilador extends MaquinaVirtual
 			rowLinhaLexema.addElement(String.valueOf(MSimbolo.get(i)));
 			rowDataLexema.addElement(rowLinhaLexema);
 		}
-		System.out.println(MSimbolo);
+		
 		columnNamesLexema.addElement("Lexema");
 		columnNamesLexema.addElement("Simbolo");
 		tabelaLexema = new JTable(rowDataLexema, columnNamesLexema);
