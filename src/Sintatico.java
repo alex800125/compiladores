a9da5812e6312953c60f594cf8317d002a672e4e
Alex de Oliveira Venturini <alex800125@hotmail.com>
2020-10-14 21:09:44 -0300

    funcionando quase tudo do lexico, ainda tem erros

--- src/Sintatico.java
+++ src/Sintatico.java
@@ -24,9 +24,10 @@ public class Sintatico extends MaquinaVirtual {
 	private static final long serialVersionUID = 1L;
 	Token token = null;
 	protected Vector<Token> tokens = new Vector<Token>();
+
 	public void analisadorSintatico() {
 		Lexico lexico = new Lexico();
-		lexico.InicializadorArquivo();	
+		lexico.InicializadorArquivo();
 
 		do {
 			try {
@@ -34,21 +35,27 @@ public class Sintatico extends MaquinaVirtual {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			tokens.add (new Token(token.getLexema(), token.getSimbolo(), token.getLinha() ));
-			//System.out.println("Token lexema = " + token.getLexema() + " | simbolo = " + token.getSimbolo());
-		} while (token.getSimbolo() != "Sfim");
-		//lexico.TabelaLexema();
+			
+			if (token.getLexema().equals("ERRO")) {
+				System.out.println("Token ERRO");
+				break;
+			} else {
+				tokens.add(new Token(token.getLexema(), token.getSimbolo(), token.getLinha()));
+				System.out.println("Token lexema = " + token.getLexema() + " | simbolo = " + token.getSimbolo());
+			}
+		} while (token.getSimbolo() != "Sponto");
+		// lexico.TabelaLexema();
 		TabelaSintatico();
 	}
-	public void TabelaSintatico() 
-	{
+
+	public void TabelaSintatico() {
 		// pnlPilha = new MeuJPanel();
 		rowDataSintatico = new Vector<Vector>();
 		columnNamesSintatico = new Vector<String>();
 		// pnlPilha.remove(barraRolagemLexema);
 		for (int i = 0; i < tokens.size(); i++) {
 			rowLinhaSintatico = new Vector<String>(); // limpa o vector, nao sei se eh o mais correto, pode afetar a
-													// memoria fisica
+														// memoria fisica
 			rowLinhaSintatico.addElement(String.valueOf(tokens.get(i).getLinha()));
 			rowLinhaSintatico.addElement(String.valueOf(tokens.get(i).getSimbolo()));
 			rowLinhaSintatico.addElement(String.valueOf(tokens.get(i).getLexema()));
@@ -60,22 +67,22 @@ public class Sintatico extends MaquinaVirtual {
 		columnNamesSintatico.addElement("Linha");
 		columnNamesSintatico.addElement("Lexema");
 		columnNamesSintatico.addElement("Simbolo");
-		
-		
+
 		tabelaSintatico = new JTable(rowDataSintatico, columnNamesSintatico);
 		barraRolagemSintatico = new JScrollPane(tabelaSintatico);
 		tabelaSintatico.setPreferredScrollableViewportSize(tabelaSintatico.getPreferredSize());
 		tabelaSintatico.setFillsViewportHeight(false);
 		pnlTabela.add(barraRolagemSintatico);
-		
-/*
- * texBreakPoint = new JTextArea(10, 15);
-		JScrollPane scrollableTextBreakPoint = new JScrollPane(texBreakPoint);
-		scrollableTextBreakPoint.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scrollableTextBreakPoint.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		texBreakPoint.setText("aaa"); // colocar codigo aquui
-		pnlAmostraDados.add(scrollableTextBreakPoint);
-		*/
+
+		/*
+		 * texBreakPoint = new JTextArea(10, 15); JScrollPane scrollableTextBreakPoint =
+		 * new JScrollPane(texBreakPoint);
+		 * scrollableTextBreakPoint.setHorizontalScrollBarPolicy(JScrollPane.
+		 * HORIZONTAL_SCROLLBAR_ALWAYS);
+		 * scrollableTextBreakPoint.setVerticalScrollBarPolicy(JScrollPane.
+		 * VERTICAL_SCROLLBAR_ALWAYS); texBreakPoint.setText("aaa"); // colocar codigo
+		 * aquui pnlAmostraDados.add(scrollableTextBreakPoint);
+		 */
 	}
 
 }
